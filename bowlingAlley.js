ae6305eb4e6efa0c19a597c8f9db7a056655ed82
rohinivsenthil <rohinivsenthil@gmail.com>
2020-01-20 10:30:28 +0530

    Created functionality and test case when user throws no spares/strikes

--- bowlingAlley.js
+++ bowlingAlley.js
@@ -1,18 +1,21 @@
 let totalScore = 0;
 
 function roll(pinsKnocked){
-	//totalScore += pinsKnocked;
+	totalScore = totalScore + pinsKnocked;
 }
 
 function score(){
-	return totalScore;
+	let finalScore = totalScore;
+	totalScore = 0;
+	return finalScore;
 }
 
 function bowlingGame(inputThrows){
-	// for(let i =0; i<inputThrows.length; i++){
-	// 	roll(inputThrows[i]);
-	// }
-    
+	for(let i = 0; i < inputThrows.length; i++){
+		roll(inputThrows[i]);
+	}
+
+
 	return score();
 }
 
--- bowlingAlley.test.js
+++ bowlingAlley.test.js
@@ -1,11 +1,20 @@
 const bowlingGame = require('./bowlingAlley');
 
+
 describe('the bowlingGame function', () => {
 
 	it('should return number', () => {
 		const result = bowlingGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
 		expect(typeof result).toBe('number');
 	});
+    
+	it('should return the sum of throws when there are no spares/strikes', () => {
+		const result = bowlingGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+		expect(result).toBe(90);
+	});
+    
+    
+
     
 
 });
