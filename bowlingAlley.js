267f8a333505f4f6ccecf1113c68f0b5dfed6ef1
rohinivsenthil <rohinivsenthil@gmail.com>
2020-01-20 11:04:34 +0530

    Created testcase and functionality for when user throws a strike

--- bowlingAlley.js
+++ bowlingAlley.js
@@ -1,4 +1,5 @@
 let totalScore = 0;
+let totalThrows = 20;
 
 function roll(pinsKnocked){
 	totalScore = totalScore + pinsKnocked;
@@ -11,8 +12,18 @@ function score(){
 }
 
 function bowlingGame(inputThrows){
-	for(let i = 0; i < inputThrows.length; i++){
-		roll(inputThrows[i]);
+	for(let i = 0; i < totalThrows; i++){
+
+		if(inputThrows[i] == 10){
+			totalThrows--;
+			roll(inputThrows[i]);
+			roll.call(null, inputThrows[i+1]);
+			roll.call(null, inputThrows[i+2]);
+		}
+
+		else{
+			roll(inputThrows[i]);
+		}
 	}
 
 
--- bowlingAlley.test.js
+++ bowlingAlley.test.js
@@ -12,6 +12,17 @@ describe('the bowlingGame function', () => {
 		const result = bowlingGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
 		expect(result).toBe(90);
 	});
+
+	it('should return the expected throw when a strike is made: add the next two throw scores to current score', () => {
+		const result = bowlingGame([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]);
+		expect(result).toBe(30);
+	});
+
+	//different test case: when strike is thrown not in the end
+	it('should return the expected throw when a strike is made: add the next two throw scores to current score', () => {
+		const result = bowlingGame([0, 0, 10, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+		expect(result).toBe(20);
+	});
     
     
 
